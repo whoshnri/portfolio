6ff6d9c31ae771fe0d4df59dfffa7e5807157687
whoshnri <henrybassey2007@gmail.com>
2025-09-05 14:10:33 +0100

    new bio

--- app/About.tsx
+++ app/About.tsx
@@ -3,20 +3,15 @@ export default function About() {
     <section className="max-w-3xl mx-auto px-4 py-16">
       <h2 className="text-3xl font-bold mb-6">About Me</h2>
       <p className="lg:text-lg text-base leading-relaxed text-gray-400 mb-4">
-        I&apos;m a passionate web developer with a love for crafting clean, efficient, 
-        and user-friendly digital experiences. My work blends creativity with 
-        technical precision, bringing ideas to life through modern tools and 
-        technologies.
+        I&apos;m a passionate slf taught developer.
       </p>
       <p className="lg:text-lg text-base leading-relaxed text-gray-400 mb-4">
-        Over the years, I&apos;ve developed skills in building scalable web applications, 
-        designing intuitive interfaces, and optimizing performance for a smooth 
-        user journey. I believe in continuous learning and staying ahead of the 
+        Over the years, I&apos;ve developed web apps, RAG pipelines for chatbots, console games with python and class projects using advance data strutures. I believe in continuous learning and staying ahead of the 
         latest industry trends.
       </p>
       <p className="lg:text-lg text-base leading-relaxed text-gray-400">
         When I&apos;m not coding, you&apos;ll find me exploring new tech concepts, contributing 
-        to open-source projects, or diving into design inspiration. I&apos;m always open 
+        to open-source projects, or diving into podcasts and music. I&apos;m always open 
         to collaboration and exciting opportunities.
       </p>
     </section>
